6a364b136482e97923f5303cc8d7ee91d552572e
efraim urel <efraimurelpalodang@gmail.com>
2024-12-05 10:32:06 +0800

    selesai membuat dashboard halaman depan untuk aplikasi halaman depan

---
+++ app/controllers/dashboard.php
@@ -0,0 +1,13 @@
+<?php
+
+class Dashboard extends Controller {
+
+  public function index()
+  {
+    // $data['bootstrap'] = '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">';
+    $data['judul'] = 'Dashboard';
+    $this->view('templates/header', $data);
+    $this->view('dashboard/index');
+    $this->view('templates/footer');
+  }
+}
