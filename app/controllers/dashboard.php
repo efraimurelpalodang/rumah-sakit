0f30778caec12734ee3309e0a6fd8094f8bbd175
efraim urel <efraimurelpalodang@gmail.com>
2024-12-05 20:21:44 +0800

    menghapus bootstrap karna belum di gunakan

--- app/controllers/dashboard.php
+++ app/controllers/dashboard.php
@@ -4,7 +4,6 @@ class Dashboard extends Controller {
 
   public function index()
   {
-    // $data['bootstrap'] = '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">';
     $data['judul'] = 'Dashboard';
     $data['style'] = BASEURL . 'css/styleDashboard.css';
     $this->view('templates/header', $data);
