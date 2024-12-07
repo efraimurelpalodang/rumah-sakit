e3a8d1d6442bcccaed3359fed879196a44b7bd06
efraim urel <efraimurelpalodang@gmail.com>
2024-12-07 18:36:31 +0800

    ga tau apa jadinya tablenya soalnya ga ketemu apa yang salah di table pasiennya kayaknya tertimpa sama style css lain deh

--- app/views/pasien/index.php
+++ app/views/pasien/index.php
@@ -2,21 +2,23 @@
   <thead>
     <tr>
       <th>No.</th>
-      <th>Nomor Rekam Medis</th>
       <th>Nama</th>
+      <th>Nomor Rekam Medis</th>
       <th>Jenis Kelamin</th>
       <th>No Handphone</th>
-      <th>Action</th>
+      <th colspan="3">Action</th>
     </tr>
   </thead>
   <tbody>
     <tr>
       <td>1</td>
-      <td>RM2018020700011</td>
       <td>Erik Budiyanto</td>
+      <td>RM2018020700011</td>
       <td>Laki-Laki</td>
       <td>085209029883</td>
-      <td><a href=""><i class='bx bx-edit bx-sm'></i><i class='bx bx-folder-minus bx-sm'></i></a></td>
+      <td ><a href=""><i class='bx bxs-user-detail bx-sm'></i></a></td>
+      <td><a href=""><i class='bx bx-edit bx-sm'></i></a></td>
+      <td><a href=""><i class='bx bx-folder-minus bx-sm'></i></a></td>
     </tr>
   </tbody>
 </table>
