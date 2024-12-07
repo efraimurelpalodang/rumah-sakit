28e2b8e983958a8b951b9c4bc11343ce8bf378db
efraim urel <efraimurelpalodang@gmail.com>
2024-12-08 06:35:53 +0800

    ga tau males dah

--- app/views/pasien/index.php
+++ app/views/pasien/index.php
@@ -1,24 +1,25 @@
 <table>
   <thead>
     <tr>
-      <th>No.</th>
-      <th>Nama</th>
-      <th>Nomor Rekam Medis</th>
+      <th>No</th>
+      <th>Nama Pasien</th>
+      <th>No Rekam Medis</th>
       <th>Jenis Kelamin</th>
-      <th>No Handphone</th>
-      <th colspan="3">Action</th>
+      <th>Tanggal Masuk</th>
+      <th>Action</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <td>1</td>
-      <td>Erik Budiyanto</td>
-      <td>RM2018020700011</td>
+      <td>1.</td>
+      <td>Erik Dermawan</td>
+      <td>RM0892989367</td>
       <td>Laki-Laki</td>
-      <td>085209029883</td>
-      <td ><a href=""><i class='bx bxs-user-detail bx-sm'></i></a></td>
-      <td><a href=""><i class='bx bx-edit bx-sm'></i></a></td>
-      <td><a href=""><i class='bx bx-folder-minus bx-sm'></i></a></td>
+      <td>09-11-2024</td>
+      <td>
+        <a href=""><i class='bx bx-edit bx-sm'></i></a>
+        <a href=""><i class='bx bx-folder-minus bx-sm'></i></a>
+      </td>
     </tr>
   </tbody>
 </table>
